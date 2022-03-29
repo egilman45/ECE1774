392e673e50609aebac2b0656932bd225a1e59194
egilman45 <55217606+egilman45@users.noreply.github.com>
2022-03-28 20:05:33 -0400

    Add files via upload

--- ECE1774_Homework2.m
+++ ECE1774_Homework2.m
@@ -53,10 +53,10 @@ Y_lines0 = [1/0.09 1/0.09 1/0.09 1/0.03 0 1/0.08 1/0.2 1/0.2 1/0.2];
 diagMatrix1 = find_diag_elements(line_bus_connections, Y_lines1, N, numLines);
 
 %Find Outside Diaglonal Elements of Ybus
-off_diag1 = find_off_diag_elements(bus_connections, Y_lines1, numLines, N);
+off_diag1 = find_off_diag_elements(line_bus_connections, Y_lines1, numLines, N);
 
 %Find the Ybus
-Ybus = find_Ybus_matrix(diagMatrix1, off_diag1, bus_connections, N);
+Ybus1 = find_Ybus_matrix(diagMatrix1, off_diag1, bus_connections, N);
 
 Zbus1 = zeros(N,N);
 
@@ -66,10 +66,53 @@ for i = 1:N
    end
 end
 
+disp('Positive Sequence Matrix:')
+disp(Zbus1)
+
 %Negative Sequence Impedance
+
+%Find the Diagonal Elements of the Ybus
+diagMatrix2 = find_diag_elements(line_bus_connections, Y_lines2, N, numLines);
+
+%Find Outside Diaglonal Elements of Ybus
+off_diag2= find_off_diag_elements(line_bus_connections, Y_lines2, numLines, N);
+
+%Find the Ybus
+Ybus2 = find_Ybus_matrix(diagMatrix2, off_diag2, bus_connections, N);
+
+Zbus2 = zeros(N,N);
+
+for i = 1:N
+   for j = 1:N
+       Zbus2(i,j) = 1/(Ybus2(i,j));
+   end
+end
+
+disp('Negative Sequence Matrix:')
+disp(Zbus2)
+
 
 %Zero Sequence Impedance
 
+%Find the Diagonal Elements of the Ybus
+diagMatrix0 = find_diag_elements(line_bus_connections, Y_lines0, N, numLines);
+
+%Find Outside Diaglonal Elements of Ybus
+off_diag0 = find_off_diag_elements(line_bus_connections, Y_lines0, numLines, N);
+
+%Find the Ybus
+Ybus0 = find_Ybus_matrix(diagMatrix0, off_diag0, bus_connections, N);
+
+Zbus0 = zeros(N,N);
+
+for i = 1:N
+   for j = 1:N
+       Zbus0(i,j) = 1/(Ybus0(i,j));
+   end
+end
+
+disp('Zero Sequence Matrix:')
+disp(Zbus0)
 
 %%%%%%%%%%Functions%%%%%%%%%%%%%
 function Zbus = find_Zbus(Ybus)
@@ -102,22 +145,13 @@ function off_diag = find_off_diag_elements(line_bus_connections, Y_lines, numLin
 
     for i = 1:numLines
         count = 0;
-        while count < 1
-            for k = 1:N
-                if(line_bus_connections(k,i)==1)
-                    bus1 = k;
-                    count = count + 1;
-                end
-            end
-        end
-        
-        count = 0;
-        while count < 1
-            for h = 1:N
-                if(line_bus_connections(h,i)==1 && h ~= bus1)
-                    bus2 = h;
-                    count = count + 1;
-                end
+        for k = 1:N
+            if(line_bus_connections(k,i)==1 && count == 0)
+                bus1 = k;
+                count = count + 1;
+            elseif(line_bus_connections(k,i)==1 && count == 1)
+                bus2 = k;
+                count = count + 1;
             end
         end
         
